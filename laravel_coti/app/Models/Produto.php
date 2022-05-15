2d3fa2577e47c333419ff700001903252158d257
Lenito <sistema@cooder.com.br>
2022-05-15 14:41:32 -0300

    bancocriado

---
+++ laravel_coti/app/Models/Produto.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/*
+extend adiciona todo conteúdo desenvolvido de 
+Model para a classe q a cita
+
+Ex:
+class Produto extends Model
+O conteúdo de Model será adicionado abaixo da Produto
+assim não preciso recriar tudo novamente
+
+**classe abstrata é um esqueleto / serve como contrato
+
+*/
+
+class Produto extends Model
+{
+    use HasFactory;
+
+    /*o laravel reconhece nome da classe e adiciona S ou IES para o plural e 
+    este será o nome da tabela.
+    o protected $table não é obrigatório, mas o Laravel em PT-BR pode errar
+    */
+    protected $table = 'produtos';
+
+    //colunas manipuláveis
+    protected $fillable = [
+        'nome',
+        'preco',
+        'descricao', 
+    ];
+}
---
+++ laravel_coti/database/migrations/2022_05_15_162542_create_produtos_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //blueprint ajuda a criar campos no DB
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('nome');
+            $table->integer('preco'); //10000 sem ponto ou vírgula
+            $table->string('descricao');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
